05c028f781c4e1d452158ecbdcb9bbd6d0966290
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-04-01 18:09:12 +0900

    Added sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SessionsController < Devise::SessionsController
+      respond_to :json
+
+      private
+
+      def respond_with(resource, _opts = {})
+        render json: {
+          message: 'Logged in successfully',
+          user: resource,
+          token: request.env['warden-jwt_auth.token']
+        }, status: :ok
+      end
+
+      def respond_to_on_destroy
+        head :no_content
+      end
+    end
+  end
+end
